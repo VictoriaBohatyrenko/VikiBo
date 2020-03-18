80a5ca4000cd76c412a4e961c42b793d2cfea7ed
Viktoria <victoriabohatyrenko@gmail.com>
2020-03-18 22:27:42 +0200

    Add project

---
+++ VikiboAutomation/VikiboAutomation/Program.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace VikiboAutomationST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создаем хром драйвер
+            var driver = new ChromeDriver();
+
+            //делаем окно браузера во весь экран
+            driver.Manage().Window.Maximize();
+
+            //устанавливаем время ожидания появления элементов (кнопок, полей и т.д.) на странице (30 секунд)
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+            //переходим на сайт
+            driver.Navigate().GoToUrl("https://www.instagram.com/");
+
+            //находим элемент (кнопка вход) по XPath и нажимаем на него
+            driver.FindElement(By.XPath("//a[text() = 'Вход']")).Click();
+
+            // находим элемент (поле ввода логин) по XPath и вводим в него текст
+            driver.FindElement(By.Name("username")).SendKeys("likedream277@gmail.com");
+
+            // находим элемент (поле ввода пароль) по XPath и вводим в него текст
+            driver.FindElement(By.Name("password")).SendKeys("192837465");
+
+            //находим элемент (кнопка вход) по XPath и нажимаем на него
+            driver.FindElement(By.XPath("//button[@type = 'submit']")).Click();
+
+            //находим элемент (книпка не сейчас) по XPath и нажимаем на него
+            driver.FindElement(By.XPath("//button[text() = 'Не сейчас']")).Click();
+
+            //находим элемент (поле поиска) по XPath и вводим в него текст
+            driver.FindElement(By.XPath("//input[@placeholder  = 'Поиск']")).SendKeys("#travel");
+
+            //Нажимаем еа кнопку по XPath 
+            driver.FindElement(By.XPath("//a[@href = '/explore/tags/travel/']")).Click();
+
+            //Нажимаем на фото
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VikiboAutomation/VikiboAutomation/VikiboAutomation/Program.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace VikiboAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создаем хром драйвер
+            var driver = new ChromeDriver();
+
+            //делаем окно браузера во весь экран
+            driver.Manage().Window.Maximize();
+
+            //устанавливаем время ожидания появления элементов (кнопок, полей и т.д.) на странице (30 секунд)
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+            //переходим на сайт
+            driver.Navigate().GoToUrl("https://www.the-village.com.ua/");
+
+            //находим элемент (кнопка поиска) по XPath и нажимаем на него
+            driver.FindElement(By.XPath("//div[@role = 'toggle_search']")).Click();
+
+            //находим элемент (строка поиска) по Name и вводим в него текст поиска
+            driver.FindElement(By.Name("query")).SendKeys("Глінтвейн та хот - доги по 33 грн");
+
+            //находим элемент (кнопка Найти) по XPath и нажимаем на него
+            driver.FindElement(By.XPath("//button[@type = 'submit']")).Click();
+
+            //находим первую статью в ленте по XPath и нажимаем по ней
+            driver.FindElement(By.XPath("//a[h2[text() = 'Глінтвейн та хот-доги по 33 грн у «Цветочная Мафия» на Майдані']]")).Click();
+
+            Console.ReadLine();
+        }
+    }
+}
